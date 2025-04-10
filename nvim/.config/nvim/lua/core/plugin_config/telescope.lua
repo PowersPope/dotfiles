a0ce38a9d4f36dba0b490d34d45d4f4c60b700b7
Andrew Powers <apowers4@uoregon.edu>
2025-04-10 12:35:00 -0700

    <FEAT> Update NVIM Config + Add Hyprpaper

---
+++ nvim/.config/nvim/lua/core/plugin_config/telescope.lua
@@ -0,0 +1,5 @@
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<Space><Space>', builtin.oldfiles, { desc = 'Telescope old files' })
+vim.keymap.set('n', '<Space>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<Space>fh', builtin.help_tags, { desc = 'Telescope help tags' })
