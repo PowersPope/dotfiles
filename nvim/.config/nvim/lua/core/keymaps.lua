1670f48f585bb7b2f017824b4bd4d8a9f37fe496
Andrew Powers <apowers4@uoregon.edu>
2025-04-23 17:40:08 -0700

    <FEAT> Add comment/uncomment keybinds for neovim * Added in custom lua functions for commenting out based on filetype in   neovim

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -22,3 +22,38 @@ vim.opt.expandtab = true
 -- Remap of commands
 -- Unhiglight searched text
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+-- Set up commenting out. This is custom and from here:
+-- https://slar.se/comment-and-uncomment-code-in-neovim.html
+local non_c_line_comments_by_filetype = {
+  lua = "-- ",
+  python = "# ",
+  xml = "<--",
+}
+
+local function comment_out(opts)
+  local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or "// "
+  local start = math.min(opts.line1, opts.line2)
+  local finish = math.max(opts.line1, opts.line2)
+  vim.api.nvim_command(start .. "," .. finish .. "s:^:" .. line_comment .. ":")
+  vim.api.nvim_command("noh")
+end
+
+local function uncomment(opts)
+  local line_comment = non_c_line_comments_by_filetype[vim.bo.filetype] or "// "
+  local start = math.min(opts.line1, opts.line2)
+  local finish = math.max(opts.line1, opts.line2)
+
+  pcall(vim.api.nvim_command, start .. "," .. finish .. "s:^\\(\\s\\{-\\}\\)" .. line_comment .. ":\\1:")
+  vim.api.nvim_command("noh")
+end
+
+-- Set comment/uncomment keybinds
+vim.api.nvim_create_user_command("CommentOut", comment_out, { range = true })
+vim.keymap.set('v', '<leader>/', ':CommentOut<CR>')
+vim.keymap.set('n', '<leader>/', ':CommentOut<CR>')
+
+vim.api.nvim_create_user_command("Uncomment", uncomment, { range = true })
+vim.keymap.set('v', '<leader>uc', ':Uncomment<CR>')
+vim.keymap.set('n', '<leader>uc', ':Uncomment<CR>')
+
