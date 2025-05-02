65cd151fe5718c95fb4e64514b18cdd226acaefd
Andrew Powers <apowers4@uoregon.edu>
2025-05-02 11:47:07 -0700

    <FIX> Edit telescopes command <C-p> to <leader>p and fixed new window creation in tmux.conf

--- nvim/.config/nvim/lua/core/plugin_config/telescope.lua
+++ nvim/.config/nvim/lua/core/plugin_config/telescope.lua
@@ -1,5 +1,5 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>p', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, { desc = 'Telescope old files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
